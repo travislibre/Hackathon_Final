b61af70cda208b3f390bb678497ecd675fdf7da5
Travis Libre <travisjlibre@gmail.com>
2022-02-26 21:14:59 -0600

    final puush

--- 008_cascade_classifier/cascadeutils.py
+++ 008_cascade_classifier/cascadeutils.py
@@ -35,3 +35,9 @@ def generate_negative_description_file():
 
 # my final classifier training arguments:
 # $ C:/Users/Ben/learncodebygaming/opencv/build/x64/vc15/bin/opencv_traincascade.exe -data cascade/ -vec pos.vec -bg neg.txt -precalcValBufSize 6000 -precalcIdxBufSize 6000 -numPos 200 -numNeg 1000 -numStages 12 -w 24 -h 24 -maxFalseAlarmRate 0.4 -minHitRate 0.999
+
+
+#$ C:/Users/tjlib/Downloads/thing2/opencv/build/x64/vc15/bin/opencv_annotation.exe --annotations=pos.txt --images=positive/
+#$ C:/Users/tjlib/Downloads/thing2/opencv/build/x64/vc15/bin/opencv_createsamples.exe -info pos.txt -w 24 -h 24 -num 1000 -vec pos.vec
+#$ C:/Users/tjlib/Downloads/thing2/opencv/build/x64/vc15/bin/opencv_traincascade.exe -data cascade/ -vec pos.vec -bg neg.txt -precalcValBufSize 6000 -precalcIdxBufSize 6000 -numPos 3 -numNeg 1000 -numStages 12 -w 10 -h 10 -maxFalseAlarmRate 0.4 -minHitRate 0.999
+#$ python main.py
